4076b073bfe9c29e500077352a2c19bf2d1df312
Fabio <fabioalvesneves@gmail.com>
2023-10-17 19:33:08 -0300

    commit-inicial

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging; // You need to include the ILogger namespace
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+using WebApi_RabbitMQ.Domain;
+
+namespace WebApi_RabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger; // Make the logger readonly
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult InsertOrder(Order order)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "orderQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "orderQueue",
+                                         basicProperties: null,
+                                         body: body);
+
+                }
+
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Não foi possível criar este pedido", ex);
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+       
+    }
+}
